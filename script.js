17e4cfe9354e95d968bf107b2702cb472ebb13d4
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-01-12 15:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-
+ str = str.toLowerCase();
+    // reverse the string
+    let reversed = str.split('').reverse().join('');
+    // check if the reversed string is equal to the original string
+    return str === reversed;
 }
 module.exports = palindrome
